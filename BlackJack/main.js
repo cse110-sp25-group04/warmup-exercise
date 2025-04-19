6307f3aa1cea39d66c9443d98bbad9859f9ada99
Eric Nguyen <ern005@ucsd.edu>
2025-04-18 23:38:41 -0700

    Update possible card ranks (New fixes)

--- BlackJack/main.js
+++ BlackJack/main.js
@@ -151,7 +151,7 @@ class Card {
       }
 }
 
-const rank = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 const suit = ["H", "D", "C", "S"];
 
 function dealCard(person) {
