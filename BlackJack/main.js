356ecad6c3cd6cf0591fb322ab65ca85d48a383c
Arul Mathur <arulster17@gmail.com>
2025-04-19 00:45:49 -0700

    removed string literal referenced to game phase

--- BlackJack/main.js
+++ BlackJack/main.js
@@ -242,7 +242,7 @@ function main() {
 
     //READY BUTTON
     document.getElementById("readyButton").addEventListener("click", () => {
-        if (gameState.currentState === "BETTING_PHASE" && gameState.READY_available) {
+        if (gameState.currentState === GameState.bettingPhase && gameState.READY_available) {
             //check if player has not bet yet
             if (player.bet == 0){
                 console.log("You must bet money to play!");
@@ -296,7 +296,7 @@ function main() {
 
     //HIT BUTTON
     document.getElementById("hitButton").addEventListener("click", () => {
-        if (gameState.currentState === "PLAYER_PHASE" && gameState.HIT_available) {
+        if (gameState.currentState === GameState.PLAYER_PHASE    && gameState.HIT_available) {
             console.log("Player hits!");
             //Make sure player can no longer double after standing
             gameState.DOUBLE_available = false;
@@ -313,7 +313,7 @@ function main() {
 
     //STAND BUTTON
     document.getElementById("standButton").addEventListener("click", () => {
-        if (gameState.currentState === "PLAYER_PHASE" && gameState.STAND_available) {
+        if (gameState.currentState === GameState.PLAYER_PHASE && gameState.STAND_available) {
             console.log("Player stands! with score: " + player.currentScore);
             playToDeal();
 
@@ -323,7 +323,7 @@ function main() {
 
     //DOUBLE BUTTON
     document.getElementById("doubleButton").addEventListener("click", () => {
-        if (gameState.currentState === "PLAYER_PHASE" && gameState.DOUBLE_available) {
+        if (gameState.currentState === GameState.PLAYER_PHASE && gameState.DOUBLE_available) {
             console.log("Player doubles!");
             // add card, check for bust etc
         }
